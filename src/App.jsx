afc78826b3154bb346fbdba002f244aa89aefb26
Logan Mclachlan <logan.g.mclachlan@gmail.com>
2024-06-03 14:37:09 +0100

    merged trivia and salutes tabs

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import {useState, lazy, Suspense} from 'react'
 import DiveData from './components/DiveData'
-const Salutes = lazy(() => import('./components/Salutes'))
+import Navbar from './components/Navbar'
 const Trivia = lazy(() => import('./components/Trivia'))
 const Information = lazy(() => import('./components/Information'))
+const DiveRecords = lazy(() => import('./components/DiveRecords'))
 
 export default function App() {
   const [tabSelected, setTabSelected] = useState("DiveData")
 
   async function getData(url){
       return await fetch(url)
-      .then(res => res.json())
-      .then(data => {return data})
-      .catch(err => console.log(err))
+        .then(res => res.json())
+        .then(data => {return data})
+        .catch(err => console.log(err))
   }
 
   return (
@@ -21,39 +22,18 @@ export default function App() {
     </div>
 
     <div className='body'>
-      <table className='tab-container'>
-        <thead>
-          <tr>
-          {tabSelected === "DiveData"
-            ? <th className='tab selected'><h2>Dive Data</h2></th>
-            : <th className='tab' onClick={() => {setTabSelected("DiveData")}}><h2>Dive Data</h2></th>
-          }
-          {tabSelected === "Trivia"
-            ? <th className='tab selected'><h2>Trivia</h2></th>
-            : <th className='tab' onClick={() => {setTabSelected("Trivia")}}><h2>Trivia</h2></th>
-          }
-          {tabSelected === "Salutes"
-            ? <th className='tab selected'><h2>Salutes</h2></th>
-            : <th className='tab' onClick={() => {setTabSelected("Salutes")}}><h2>Salutes</h2></th>
-          }
-          {tabSelected === "Info"
-            ? <th className='tab selected'><h2>Info</h2></th>
-            : <th className='tab' onClick={() => {setTabSelected("Info")}}><h2>Info</h2></th>
-          }
-          </tr>
-        </thead>
-      </table>
+      <Navbar tabSelected={tabSelected} setTabSelected={setTabSelected}/>
       
       <div className='content'>
         {tabSelected === "DiveData" &&
           <DiveData getData={() => getData("https://drgapi.com/v1/deepdives")}/>
         }
         <Suspense fallback="Loading...">
-        {tabSelected === "Trivia" &&
-          <Trivia getData={() => getData("https://drgapi.com/v1/trivia")}/>
+        {tabSelected === "DiveRecords" &&
+          <DiveRecords/>
         }
-        {tabSelected === "Salutes" &&
-          <Salutes getData={() => getData("https://drgapi.com/v1/salutes")}/>
+        {tabSelected === "Trivia" &&
+          <Trivia getData={getData}/>
         }
         {tabSelected === "Info" &&
           <Information/>
--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,19 +1,28 @@
+import image1 from '../assets/rock-&-stone.jpg'
+import image2 from '../assets/smart-stout.jpg'
 import {useState} from 'react'
-import image from '../assets/smart-stout.jpg'
 
-export default function Trivia({getData}){
+export default function trivia({getData}){
     const [trivia, setTrivia] = useState(null)
 
+    async function rockOn(){
+        let data = await getData("https://drgapi.com/v1/salutes")
+        alert(data.salutes[Math.floor(Math.random() * data.salutes.length)])
+    }
+
     async function getTrivia(){
-        let data = await getData()
+        let data = await getData("https://drgapi.com/v1/trivia")
         setTrivia(data.trivia[Math.floor(Math.random() * data.trivia.length)])
     }
 
     return(
-        <>
-            <img src={image} onClick={getTrivia} className='image'/><br/>
-            <button onClick={getTrivia} className='big-button'>Bottoms Up!</button>
-            <p>{trivia}</p>
-        </>
+    <>
+        <img src={image2} onClick={getTrivia} className='image'/><br/>
+        <button onClick={getTrivia} className='big-button'>Bottoms Up!</button>
+        <p>{trivia}</p>
+
+        <img onClick={rockOn} className="image" src={image1}/><br/>
+        <button onClick={rockOn} className="big-button">Rock and Stone?</button>
+    </>
     )
 }
