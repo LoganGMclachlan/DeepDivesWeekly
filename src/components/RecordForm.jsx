eea69248146279e72cd4ad1c703b5b80f757f94b
Logan Mclachlan <logan.g.mclachlan@gmail.com>
2024-06-10 13:25:32 +0100

    record form now accepts player data

--- src/components/RecordForm.jsx
+++ src/components/RecordForm.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react"
+import "./recordForm.css"
 
 export default function RecordForm({setShowForm}){
     const [time,setTime] = useState({"H":"","M":"","S":""})
+    const [team,setTeam] = useState([])
+    const [playerName,setPlayerName] = useState("")
+    const [playerClass,setPlayerClass] = useState("Scout")
 
     const setHours = value => {if(value >= 0 && value <= 3 && value !== " "){
         setTime({...time,"H":value})}}
@@ -9,6 +13,17 @@ export default function RecordForm({setShowForm}){
         setTime({...time,"M":value})}}
     const setSeconds = value => {if(value >= 0 && value <= 59 && value !== " "){
         setTime({...time,"S":value})}}
+
+    const addPlayer = () => {
+        if(team.length === 4){alert("Cannot add more than 4 players to a team.");return}
+        if(playerName === ""){alert("A player name is required.");return}
+        setTeam(team.concat({"name":playerName,"class":playerClass}))
+        setPlayerName("")
+    }
+
+    const removePlayer = index => {
+        setTeam(team.filter((_,i) => i !== index))
+    }
 
     return(
     <form className="record-form">
@@ -26,20 +41,34 @@ export default function RecordForm({setShowForm}){
         </>
         <>
             <h2>The Team</h2>
+            {team.length > 0 && 
+                <table>
+                    <tr>
+                        <th>Name</th>
+                        <th>Class</th>
+                    </tr>
+                    {team.map(player => <tr>
+                        <td>{player.name}</td>
+                        <td>{player.class}</td>
+                        <td style={{"width":"70px","cursor":"pointer"}}
+                            onClick={() => removePlayer(team.indexOf(player))}>Remove</td>
+                    </tr>)}
+                </table>
+            }
             <div>
                 <label>Player Name:</label>
-                <input/>
+                <input value={playerName} onChange={e => setPlayerName(e.target.value)}/>
                 <label>Class:</label>
-                <select>
-                    <option value="Scout">Scout</option>
+                <select onChange={e => setPlayerClass(e.target.value)}>
+                    <option default value="Scout">Scout</option>
                     <option value="Driller">Driller</option>
                     <option value="Gunner">Gunner</option>
                     <option value="Engineer">Engineer</option>
                 </select>
-                <b style={{"cursor":"pointer"}}>Add Player</b>
+                <b style={{"cursor":"pointer"}} onClick={addPlayer}>Add Player</b>
             </div>
         </>
-        <hr/>
+        <br/>
         <button type="submit">Submit</button>
         <button onClick={() => setShowForm(false)}>Cancel</button>
     </form>
