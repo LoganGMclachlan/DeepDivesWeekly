94a9350ff5dd63712171c3f6a5b1c630abacb4ed
Logan Mclachlan <logan.g.mclachlan@gmail.com>
2025-04-22 00:16:36 +0100

    seperates records into tabs of 5 items each and buttons to switch between them

--- src/components/DiveRecords.jsx
+++ src/components/DiveRecords.jsx
@@ -5,7 +5,8 @@ import Record from "./Record"
 import RecordForm from "./RecordForm"
 
 export default function DiveRecords(){
-    const [records, setRecords] = useState([{"Title":"Loading Records..."}])
+    const [records, setRecords] = useState([[{"Title":"Loading Records..."}]])
+    const [recordTab, setRecordTab] = useState(0)
     const [showForm, setShowForm] = useState(false)
 
     useEffect(() => {getRecords()}, [])
@@ -13,17 +14,38 @@ export default function DiveRecords(){
     const getRecords = useCallback(async () => {
         try{
             let rawData = await getDocs(collection(db,"Dive Records"))
-            setRecords(rawData.docs.map(doc => ({
+            let mappedData = rawData.docs.map(doc => ({
                 ...doc.data(), id: doc.id
-            })))
+            }))
+        setRecords(chunkList(mappedData,4))
         }
         catch(err){console.error(err)}
     })
 
+    const chunkList = (list,size) => {
+        return Array.from({ length: Math.ceil(list.length / size) }, (v, i) =>
+            list.slice(i * size, i * size + size)
+        )
+    }
+
     return(
     <>
         <div className="record-list">
-            {records.map(record => <Record record={record}/>)}
+            {records[recordTab].map(record => <Record record={record}/>)}
+
+            <div className="record-tab-button-container">
+                {records.length > 1 && <>
+                    {recordTab > 0 ? <button className="record-btn" onClick={() => setRecordTab(recordTab-1)}>
+                        Page {recordTab}</button>
+                        : <button className="record-btn" onClick={() => setRecordTab(records.length-1)}>
+                        Page {records.length}</button>}
+                    {recordTab < records.length-1 ? <button className="record-btn" onClick={() => setRecordTab(recordTab+1)}>
+                        Page {recordTab+2}</button>
+                        : <button className="record-btn" onClick={() => setRecordTab(0)}>
+                        Page 1</button>}
+                </>}
+            </div>
+
             <button onClick={() => setShowForm(true)}
                 className="record-btn" style={{"width":"100%"}}>Add Record</button>
         </div>
